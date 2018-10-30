1c46b4a820f3d66f7068873836b5405c82658b7c
brain.duan <brain.duan@analyticservice.net>
2018-10-30 16:26:22 +0800

    add login call back

---
+++ client/login.js
@@ -0,0 +1,28 @@
+function go() {
+  var boardname = document.forms[0].elements["boardname"].value;
+  var username = document.forms[0].elements["username"].value;
+  var pass = document.forms[0].elements["pass"].value;
+ if(boardname==''||username==''||pass==''){
+    return false
+ }
+ $.ajax({
+     url:'/login',
+     dataType:'json',
+     data:{
+         boardname:boardname,
+         username:username,
+         password:pass
+     },
+     success:function(d){
+        if(d.msg &&  d.msg==='OK' &&d.token){
+            window.location.href=escape(boardname)+'?token='+d.token
+        }
+    },
+     error:function(e){
+         console.log(e)
+     }
+ })
+  //   value = escape(boardname);
+//   window.location.href = value;
+  return false;
+}
